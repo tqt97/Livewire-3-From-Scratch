99ebca745174ca37cadc2eff11fecad5fc96c673
QuocTuanIT <kutuanonline199@gmail.com>
2023-10-29 09:59:44 +0700

    Practical Start: Form Live-Submit

---
+++ app/Livewire/CreatePost.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Rule;
+
+class CreatePost extends Component
+{
+    #[Rule('required|min:5')]
+    public string $title = '';
+
+    #[Rule('required|min:5')]
+    public string $body = '';
+
+    public bool $success = false;
+
+    public function save(): void
+    {
+        $this->validate();
+
+        // Post::create([
+        //     'title' => $this->title,
+        //     'body' => $this->body,
+        // ]);
+
+        $this->success = true;
+
+        $this->reset('title', 'body');
+    }
+
+    public function render(): View
+    {
+        return view('livewire.create-post');
+    }
+}
